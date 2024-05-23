1e5f7f295a3e19f1f7f6d0a30f3cbd2292b114d5
canhvanlam <canhvanlam@gmail.com>
2024-05-23 08:45:00 +0700

    change Routes

--- src/middleware.js
+++ src/middleware.js
@@ -2,12 +2,11 @@
 import { NextResponse } from 'next/server'
 import {STORAGE_KEY} from "@/constants/application.constant"
 import ROUTES from './constants/routes';
-const protectedRoutes = ["/"]
 export function middleware(request, context) {
     let verify = request.cookies.getAll();
     const accessToken = request.cookies.get(STORAGE_KEY.ACCESS_TOKEN) ;
     let url = request.url
-    if(!accessToken && protectedRoutes.includes(request.nextUrl.pathname)){
+    if(!accessToken && ROUTES.HOME.includes(request.nextUrl.pathname)){
         const absoluteURL = new URL(ROUTES.LOGIN, request.nextUrl.origin);
         return NextResponse.redirect(absoluteURL.toString());
     }
